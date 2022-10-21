f39eee9d5d31729e19a76041bc7744d6e10ab7aa
Pragyan Boruah <progyan.rdmi4@gmail.com>
2022-10-21 16:31:41 +0530

    Added login and sign up.

--- app/src/main/java/com/example/healthyfy/MainActivity.kt
+++ app/src/main/java/com/example/healthyfy/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.example.healthyfy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.healthyfy.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val firebaseUser= firebaseAuth.getCurrentUser()
+        if (firebaseUser != null) {
+            finish()
+            startActivity(Intent(this@MainActivity, Homepage::class.java))
+        }
+        binding.signinBtn.setOnClickListener{
+            val intent = Intent(this@MainActivity, SignUp::class.java)
+            startActivity(intent)
+        }
+        binding.forgotpassword.setOnClickListener{
+//            val intent = Intent(this@MainActivity,ForgotPassword::class.java)
+//            startActivity(intent)
+        }
+        binding.loginBtn.setOnClickListener{
+            val mail = binding.loginEmail.getText().toString().trim { it <= ' ' }
+            val password = binding.loginPassword.getText().toString()
+            if (mail.isEmpty() || password.isEmpty()) {
+                Toast.makeText(applicationContext, "All fields are required", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                //firebase work
+                firebaseAuth.signInWithEmailAndPassword(mail, password).addOnCompleteListener{ task ->
+                        if (task.isSuccessful) {
+                            checkMailVerification()
+                        } else {
+                            Toast.makeText(
+                                applicationContext,
+                                "Account doesn't exist",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun checkMailVerification() {
+        val firebaseUser= firebaseAuth.getCurrentUser()
+        if (firebaseUser != null) {
+            if (firebaseUser.isEmailVerified) {
+                Toast.makeText(applicationContext, "Logged in", Toast.LENGTH_SHORT).show()
+                finish()
+                startActivity(Intent(this@MainActivity, Homepage::class.java))
+            } else {
+                Toast.makeText(applicationContext, "verify your mail first.", Toast.LENGTH_SHORT).show()
+                firebaseAuth.signOut()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/healthyfy/SignUp.kt
@@ -0,0 +1,73 @@
+package com.example.healthyfy
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.healthyfy.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUp : AppCompatActivity() {
+    lateinit var binding:ActivitySignUpBinding
+    private var firebaseAuth: FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.loginpage.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.signupbutton.setOnClickListener{
+            val mail = binding.signupEmail.getText().toString().trim()
+            val password = binding.signupPassword.getText().toString().trim()
+            if (mail.isEmpty() || password.isEmpty()) {
+                Toast.makeText(applicationContext, "All fields are required", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (password.length < 8) {
+                Toast.makeText(
+                    applicationContext,
+                    "Password must be of at least 8 characters",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                firebaseAuth!!.createUserWithEmailAndPassword(mail, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                applicationContext,
+                                "Registration Successful",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            sendEmailVerification()
+                        } else Toast.makeText(
+                            applicationContext,
+                            "Failed to Register",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+            }
+        }
+    }
+
+    private fun sendEmailVerification() {
+        val firebaseUser = firebaseAuth!!.currentUser
+        firebaseUser?.sendEmailVerification()?.addOnCompleteListener {
+            Toast.makeText(
+                applicationContext,
+                "Verification email sent. Verify and login.",
+                Toast.LENGTH_SHORT
+            ).show()
+            firebaseAuth!!.signOut()
+            finish()
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+            ?: Toast.makeText(
+                applicationContext,
+                "Failed to send verification Email.",
+                Toast.LENGTH_SHORT
+            ).show()
+    }
+}
